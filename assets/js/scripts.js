11667c8ca41ab7fa367115906b2b92325766af62
Gerard Hynes <gkhynes@gmail.com>
2018-06-03 23:11:28 +0100

    Add animations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     }
   }),
     {
-      offset: "60"
+      offset: "60px;"
     };
 
   // Hero buttons scrolling
@@ -61,4 +61,31 @@ $(document).ready(function() {
         }
       }
     });
+
+  // Animations on scroll
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    { offset: "50%" }
+  );
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    { offset: "50%" }
+  );
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    { offset: "50%" }
+  );
 });
